43f46323e793e21afe4c62a223a3bd5b92c46091
alexiskatum <alexiskatum@hotmail.com>
2021-11-22 22:03:17 -0500

    bd code

--- mod_bd.py
+++ mod_bd.py
@@ -1,59 +1,37 @@
+from typing import Any
+
 import sqlite3
 
-# connexion sur la BD
 
-conn = sqlite3.connect('GHbd.bdf')
-cde_tab = '''create table if not existe GITable(
-              repertoire text,
-              topics text,
-              stars text,
-              description text,
-              link text,
-              version text,
-              release int,
-              sponsor text,
-              packages text,
-              user text,
-              contributors text,
-              languages text,
-              )
-              '''
+def cree_tab():
+    conn = obtenir_connexion()
+    curseur = conn.cursor()
+    cde_tab = '''create table if not existe GITable(
+                  repertoire text,
+                  stars text,
+                  topics text,
+                  description text,
+                  link text,
+                  version text,
+                  release int,
+                  sponsor text,
+                  packages text,
+                  user text,
+                  contributors text,
+                  languages text,
+                  )
+                  '''
+    curseur.execute(cde_tab)
 
-# curseur
+def obtenir_connexion():
+    return sqlite3.connect('GHbd.bdf')
 
-curseur = conn.cursor()
-
-# creer la table
-
-curseur.execute(cde_tab)
-
-# insertion de donnee
-
-cde_ins = '''insert into GITable( repertoire text,
-              topics,
-              stars,
-              description,
-              link,
-              version,
-              release,
-              sponsor,
-              packages,
-              user,
-              contributors,
-              languages) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''';
-curseur.execute(cde_ins, [a, b, c, d, e, f, g, h, i, j])
-conn.commit()
-
-# recherche
-
-
-recherche = str(input('recherche par topic, profile ou language ?:'))
-
-
-if recherche is 'topic':
-    topic_rec = str(input('topic recherche ?:'))
-    rec = '''select topics,
+def insertion_table(data):
+    conn = obtenir_connexion()
+    curseur = conn.cursor()
+    cde_ins = '''insert into GITable( repertoire text,
                   stars,
+                  topics,
                   description,
                   link,
                   version,
@@ -62,12 +40,36 @@ if recherche is 'topic':
                   packages,
                   user,
                   contributors,
-                  languages where topics =?'''
-    curseur.execute(rec, [topic_rec])
+                  languages) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''';
+    for ins in data.registre:
+        curseur.execute(cde, [ins.titre,
+                              ins.star,
+                              ins.topic,
+                              ins.description,
+                              ins.links,
+                              ins.version,
+                              ins.releases,
+                              ins.sponsors,
+                              ins.packages,
+                              ins.users,
+                              ins.contributors,
+                              ins.languages])
+    conn.commit()
+    conn.close()
 
-elif recherche is 'profile':
-    profile_rec = str(input('profile recherche ?:'))
-    rec = '''select topics,
+
+
+
+# recherche
+
+def recherche():
+    conn = obtenir_connexion()
+    curseur = conn.cursor()
+    recherche = str(input('recherche par topic, profile ou language ?:'))
+
+    if recherche is 'topic':
+        topic_rec = str(input('topic recherche ?:'))
+        rec = '''select topics,
                       stars,
                       description,
                       link,
@@ -77,33 +79,50 @@ elif recherche is 'profile':
                       packages,
                       user,
                       contributors,
-                      languages where user =?'''
-    curseur.execute(rec, [profile_rec])
+                      languages where topics =?'''
+        curseur.execute(rec, [topic_rec])
 
-    rec2 = '''select topics,
-                      stars,
-                      description,
-                      link,
-                      version,
-                      release,
-                      sponsor,
-                      packages,
-                      user,
-                      contributors,
-                      languages where contributors =?'''
-    curseur.execute(rec2, [profile_rec])
+    elif recherche is 'profile':
+        profile_rec = str(input('profile recherche ?:'))
+        rec = '''select topics,
+                          stars,
+                          description,
+                          link,
+                          version,
+                          release,
+                          sponsor,
+                          packages,
+                          user,
+                          contributors,
+                          languages where user =?'''
+        curseur.execute(rec, [profile_rec])
 
-elif recherche is 'language':
-    language_rec = str(input('languages recherche ?:'))
-    rec = '''select topics,
-                      stars,
-                      description,
-                      link,
-                      version,
-                      release,
-                      sponsor,
-                      packages,
-                      user,
-                      contributors,
-                      languages where languages =?'''
-    curseur.execute(rec, [language_rec])
+        rec2 = '''select topics,
+                          stars,
+                          description,
+                          link,
+                          version,
+                          release,
+                          sponsor,
+                          packages,
+                          user,
+                          contributors,
+                          languages where contributors =?'''
+        curseur.execute(rec2, [profile_rec])
+
+    elif recherche is 'language':
+        language_rec = str(input('languages recherche ?:'))
+        rec = '''select topics,
+                          stars,
+                          description,
+                          link,
+                          version,
+                          release,
+                          sponsor,
+                          packages,
+                          user,
+                          contributors,
+                          languages where languages =?'''
+        curseur.execute(rec, [language_rec])
+
+
